799f91605f0df6068be39fa649cd0a6ab511ed72
Rui Cheng <ktei2008@gmail.com>
2020-01-21 00:54:49 +1100

    use s3 for remote state storage

--- dev/main.tf
+++ dev/main.tf
@@ -2,6 +2,14 @@ terraform {
   required_version = ">= 0.12"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-397977497739"
+    key    = "dev/terraform.tfstate"
+    region = "ap-southeast-2"
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-2"
 }
@@ -18,9 +26,9 @@ module "sg" {
 }
 
 module "lb" {
-  source = "./lb"
-  environment = "dev"
-  public_subnets = module.vpc.public_subnets
+  source          = "./lb"
+  environment     = "dev"
+  public_subnets  = module.vpc.public_subnets
   security_groups = [module.sg.public_lb_http_sg_id]
 }
 
